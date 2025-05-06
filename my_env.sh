1cdc7b4a7c13d612472179011a9a2a3aea79847b
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-06 17:04:38 +0800

    update my_env.sh

--- my_env.sh
+++ my_env.sh
@@ -4,7 +4,7 @@
 # 配置 ｜ 仅用于AOSP源码拉取（默认GIT配置不影响运行）
 your_git_username="example"
 your_git_email="example@example.com"
-git_android_manifest="https://github.com/Project-Mist-OS/manifest"
-git_android_branch="15"
+git_android_manifest="https://github.com/RisingOS-Revived/android"
+git_android_branch="fifteen"
 # only if get status on wechat (wxpusher) | 使用微信推送请配置UID_xxx (公众号wxpusher获取)
 wxpusher_uid=""
